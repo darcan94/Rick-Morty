5ad3d3205ba8426ec32efe1e24f51cf62e5aa778
Dario Candia <darcanf.94@gmail.com>
2023-01-07 11:35:32 -0300

    Card component

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -2,7 +2,27 @@ import { LitElement, html, css, nothing } from "lit";
 
 class Card extends LitElement{
     render(){
-        return html``;
+        return html`
+            <section>
+                <div>
+                    <slot name="image"></slot>
+                </div>
+            </section>
+        `;
+    }
+
+    get infoTemplate(){
+        return html`
+            ${this.hasSlot() ? html`
+                <div>
+                    <slot name='info'></slot>
+                </div>
+            ` : nothing}
+        `;
+    }
+
+    hasSlot( name = 'info' ){
+        return this.querySelector(`[slot="${name}"]`) !== null;
     }
 
     static style = css`
